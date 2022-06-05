e4b401aa369e8b9c6ad9b45b99978f646a2887e6
semenkivv <101628506+semenkivv@users.noreply.github.com>
2022-06-05 12:07:14 +0300

    Create FileExtensionsStrings.java

---
+++ FileExtensionsStrings.java
@@ -0,0 +1,8 @@
+package com.company;
+
+
+//Класс для хранения расширений файлов
+public class FileExtensionsStrings {
+    public static final String json = ".json";
+    public static final String txt = ".txt";
+}

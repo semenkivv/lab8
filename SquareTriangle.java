03b2b36115410f654051e7ab9370002de884f161
semenkivv <101628506+semenkivv@users.noreply.github.com>
2022-06-05 11:57:08 +0300

    Create SquareTriangle.java

---
+++ SquareTriangle.java
@@ -0,0 +1,17 @@
+package com.bogdan;
+
+
+//Прямоугольный треугольник, наследованый от обычного
+public class SquareTriangle extends Triangle {
+    public SquareTriangle(double a, double b, double c) {
+        super(a, b, c);
+    }
+
+    public boolean checkSquareAngle(){
+        if(super.getA() *super.getA()+super.getB()*super.getB() == super.getC()*super.getC()){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
